df1995220f0031a5f0f3eaa5dd0b90b7c9177367
itsybybsy <162941863+itsybybsy@users.noreply.github.com>
2026-09-25 15:35:52 +1000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,18 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
 
+const firebaseConfig = {
+  apiKey: "YOUR_EXISTING_API_KEY",
+  authDomain: "spotify-5ede7.firebaseapp.com",
+  projectId: "spotify-5ede7",
+  storageBucket: "spotify-5ede7.appspot.com",
+  messagingSenderId: "264151726732",
+  appId: "1:264151726732:web:fc864ead06cd69b77a99a9",
+  measurementId: "G-7L47B510W7",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
